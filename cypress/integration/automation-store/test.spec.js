f391087853232bcbacf951725773a7a8424c36aa
Abrar Shariar <shariarabrar@gmail.com>
2021-06-01 13:38:07 +0900

    Add new tests

--- cypress/integration/automation-store/test.spec.js
+++ cypress/integration/automation-store/test.spec.js
@@ -4,5 +4,8 @@ describe('Test the automation store', () => {
 	it('show the homepage', () => {
 		cy.visit('https://automationteststore.com/');
 		cy.log("Hello World");
+		cy.get('#appendedInputButton').type("Hello World");
+		cy.get('#topnav > .form-control').select("Specials");
+		cy.url().should('include', '/special');
 	});
 });	
